05acef520ac6fc04ea38758cef0834d3e7fdf677
marked01one <minhkhoitran2k3@gmail.com>
2024-01-28 18:05:39 -0800

    404 error page & method

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	is_server := false
-	flag.BoolVar(&is_server, "server", false, "determines whether the running TCP connection is a server or client.")
+	is_client := false
+	flag.BoolVar(&is_client, "client", false, "determines whether the running TCP connection is a server or client.")
 
 	flag.Parse()
 
-	if is_server {
+	if !is_client {
 		fmt.Print("::::: RUNNING SERVER :::::\n\n")
 		utils.Server()
 	} else {
--- utils/server.go
+++ utils/server.go
@@ -8,7 +8,7 @@ import (
 	"time"
 )
 
-type request struct {
+type Request struct {
 	method string
 	path   string
 }
@@ -26,28 +26,33 @@ const host string = "localhost:8080"
 func Server() {
 	ln, err := net.Listen("tcp", host)
 	handleError(err, "Failed to bind to port 8080!")
-	// Awaiting for request from a client
-	conn, err := ln.Accept()
-	handleError(err, "Error accepting connection!")
-	fmt.Println("Connection Accepted!")
-	// Read request content from client
-	b := make([]byte, 1024*4)
-	conn.Read(b)
-	lines := strings.Fields(string(b))
-	req := request{method: lines[0], path: lines[1]}
-	// Determine response type based on request
-	switch req.path {
-	case "/":
-		getPage(conn, "./pages/html/index.html")
-	default:
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-	}
 
-	fmt.Println("Response was successful!")
-	conn.Close()
+	for {
+		// Awaiting for request from a client
+		conn, err := ln.Accept()
+		handleError(err, "Error accepting connection!")
+		fmt.Println("Connection Accepted!")
+		// Read request content from client
+		b := make([]byte, 1024*4)
+		conn.Read(b)
+		lines := strings.Fields(string(b))
+		req := Request{method: lines[0], path: lines[1]}
+
+		// Determine response type based on request
+		switch req.path {
+		case "/":
+			getHTMLPage(conn, "./pages/html/index.html")
+		case "/api":
+			getJson(conn)
+		default:
+			getError(conn, "./pages/html/errors/404.html", "404 Not Found")
+		}
+
+		fmt.Println("Response was successful!")
+	}
 }
 
-func getPage(conn net.Conn, pageDir string) {
+func getHTMLPage(c net.Conn, pageDir string) {
 	page, err := os.ReadFile(pageDir)
 	handleError(err, "Cannot extract content from HTML!")
 
@@ -61,7 +66,28 @@ func getPage(conn net.Conn, pageDir string) {
 
 	response := head.generateResponse("HTTP/1.1 200 OK")
 
-	conn.Write(response)
+	c.Write(response)
+}
+
+func getError(c net.Conn, pageDir string, status_code string) {
+	page, err := os.ReadFile(pageDir)
+	handleError(err, "Cannot get error page!")
+
+	head := Response{
+		server:         host,
+		date:           time.Now().UTC().Format(time.RFC1123),
+		content_length: len(page),
+		content_type:   "text/html",
+		content:        page,
+	}
+
+	s := fmt.Sprintf("HTTP/1.1 %s", status_code)
+	response := head.generateResponse(s)
+	c.Write(response)
+}
+
+func getJson(c net.Conn) {
+	c.Write([]byte{76, 111})
 }
 
 func (h *Response) generateResponse(response_code string) []byte {
